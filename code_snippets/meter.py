97b153239e9e2a7f38c6b0a041d959862c8105d5
Mark Wang <peiqi1122@gmail.com>
2022-05-12 15:35:34 -0400

    add

---
+++ code_snippets/meter.py
@@ -0,0 +1,85 @@
+from enum import Enum
+from collections import OrderedDict
+
+
+class Summary(Enum):
+    NONE = 0
+    AVERAGE = 1
+    SUM = 2
+    COUNT = 3
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self, name, fmt=':f', summary_type=Summary.AVERAGE):
+        self.name = name
+        self.fmt = fmt
+        self.summary_type = summary_type
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
+    def summary(self):
+        fmtstr = ''
+        if self.summary_type is Summary.NONE:
+            fmtstr = ''
+        elif self.summary_type is Summary.AVERAGE:
+            fmtstr = '{name} {avg:.3f}'
+        elif self.summary_type is Summary.SUM:
+            fmtstr = '{name} {sum:.3f}'
+        elif self.summary_type is Summary.COUNT:
+            fmtstr = '{name} {count:.3f}'
+        else:
+            raise ValueError('invalid summary type %r' % self.summary_type)
+
+        return fmtstr.format(**self.__dict__)
+
+
+class ProgressMeter(object):
+    def __init__(self, num_batches, meter_args=None, prefix=""):
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
+        if meter_args is not None:
+            meters = [(x[0], AverageMeter(*x)) for x in meter_args]
+        else:
+            meters = []
+        self.meters = OrderedDict(meters)
+        self.prefix = prefix
+
+    def update(self, k, v, n=1):
+        if isinstance(k, tuple):
+            k, fmt = k
+        else:
+            fmt = ':f'
+        if k not in self.meters:
+            self.meters[k] = AverageMeter(k, fmt=fmt)
+        self.meters[k].update(v, n=n)
+
+    def display(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for k, meter in self.meters.items()]
+        print('\t'.join(entries))
+
+    def display_summary(self):
+        entries = [" *"]
+        entries += [meter.summary() for k, meter in self.meters.items()]
+        print(' '.join(entries))
+
+    def _get_batch_fmtstr(self, num_batches):
+        num_digits = len(str(num_batches // 1))
+        fmt = '{:' + str(num_digits) + 'd}'
+        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
